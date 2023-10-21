6ac15f00973296123384ee9e544bde0af42cba26
Léo Zambone <138814524+LeoZamboneNeto@users.noreply.github.com>
2023-10-21 11:30:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,15 +8,6 @@ int hora;
 int escolha;
 char fechar;
 
-// MUDANÇAS:
-// Definição da struct para representar uma consulta médica
-// void Mostrar: Mudança de estrutura para beleza e visibilidade
-// void visualizar: Mudança de beleza estrutural
-// Incluir: Adição de visualização sobre códigos e planos
-// Incluir: Horário de funcionamento 7 às 20
-// Incluir:adição de loop para retornar ao cadastro
-// Horários em ordem crescente
-
 struct FichaConvenio {
 	int codigo;
 	char  nome[50];
@@ -56,9 +47,13 @@ void mostrar(struct FichaMedica *paciente) {
     //Mudança de estrutura para beleza e visibilidade
 
 	if (paciente->hora!=0){
-		printf( "|%2i       |%4s      | %9s   |  %i       |%7s       |%5i      |\n", paciente->hora, paciente->nome, paciente->celular,paciente->codigo, paciente->fichaC.nome, paciente->fichaC.valor);
-
-    	//printf ("\n-------------------------\n");
+		printf( "|%-10i| %-10s| %-15s| %-8i| %-15s| %-10i|\n",
+                  paciente->hora,
+                  paciente->nome,
+                  paciente->celular,
+                  paciente->codigo,
+                  paciente->fichaC.nome,
+                  paciente->fichaC.valor);
 		}
 	}
 
@@ -73,32 +68,23 @@ for (i=0; i<20; i++){
 }
 
 void visualizar () {
-    //modificação table para beleza estrutural
+    	printf("|%-10s| %-10s| %-15s| %-8s| %-15s| %-10s|\n", 
+		"Hora", 
+		"Nome", 
+		"Celular", 
+		"Código", 
+		"Convênio", 
+		"Valor");
 
-	   for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 12-i; j++) {
-             if (ficha[j].hora > ficha[j+1].hora) {
-                // Troca as entradas
-
-		struct FichaMedica temp = ficha[j];
-                ficha[j] = ficha[j+1];
-                ficha[j+1] = temp;
-            }
-        }
-    }
-
-	printf("\n\n|Hora     | Nome     | Celular       | Código     | Convênio     | Valor     |\n");
-	
 	for (k = 0; k < 12; k++){
-        mostrar(&ficha[k]);
-    }
+        // Exibição das informações da consulta
+    	    mostrar(&ficha[k]);
+			}
 }
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    
     bancoDadosConvenio ()  ;
-
 
     do {
     	system ("cls");
@@ -108,64 +94,74 @@ int main() {
         printf("3. Apagar\n");
         printf("4. Alterar\n");
         printf("5. Sair\n");
-        printf("Escolha uma opção: ");
-
+        
+	printf("Escolha uma opção: ");
         scanf("%d", &escolha);
-        system ("cls");
+        
+	system ("cls");
 
         switch (escolha) {
             case 1:
                 printf("Você escolheu Visualizar.\n\n");
                 visualizar ();
-			              
-              printf("Tecle algo para voltar");
-				      scanf("%c%c", &fechar, &fechar);
-              break;
+		printf("Tecle algo para voltar");
+		scanf("%c%c", &fechar, &fechar);
+		break;
 
-		
-            case 2:
+             case 2:
                 printf("Você escolheu Incluir.\n\n");
                 printf("_________________________________________________\n");
                 printf("|Planos  | Código   |  Plano       | Valor (R$) |\n");
                 printf("|_______________________________________________|\n");
 
                 for(int i = 0; i < 4; i++){
-                printf("|  %2i    |%5d     |  %-12s| %5d      |\n", i+1, convenio[i].codigo, convenio[i].nome, convenio[i].valor); }
+                printf("|  %2i    |%5d     |  %-12s| %5d      |\n", i+1, 
+			convenio[i].codigo, 
+			convenio[i].nome, 
+			convenio[i].valor);}
                 printf("|_______________________________________________|\n");
+
+                //visualizar ();
 
                 int horarioValido = 0;
                 int horarioDisponivel;
 
                 while (!horarioValido) {
-                        printf("\n\n|Hora    | Nome     | Celular      | Código     |\n");
+                        printf("\n\nHora: \n");
                         scanf("%i", &hora);
 
-                 if (hora < 7 || hora > 19){ printf("*Horário de atendimento indisponível*\n"); }
+                 if (hora < 7 || hora > 19) {printf("*Horário de atendimento indisponível*\n"); }
                  else {
-                        int horarioDisponivel = 1;                         
-			 for(int i = 0; i < 13; i++){
-                           if(ficha[i].hora == hora){
-                               horarioDisponivel = 0;
-                               break;}
+                        int horarioDisponivel = 1;
+
+                        for(int i = 0; i < 13; i++){
+                            if(ficha[i].hora == hora){
+                            horarioDisponivel = 0;
+                            break;}
                       }
 
                  if (horarioDisponivel){
                         horarioValido = 1;  // O horário é válido, podemos sair do loop
                         hora = hora - 7;
                         ficha[hora].hora = hora + 7;
+
+                        printf("Digite o nome: ");
                         scanf("%s", ficha[hora].nome);
+
+                        printf("Digite o celular: ");
                         scanf("%s", ficha[hora].celular);
+
+                        printf("Digite o código: ");
                         scanf("%i", &ficha[hora].codigo);
-                        autopreencher(hora);
-                 }
+                        
+			autopreencher(hora);}
 
-                 else {printf("Horário já cadastrado.\n"); }
-		    
-		     }// chave no final de tudo pois o primeiro else envolve todo trecho
+                 else {printf("Horário já cadastrado.\n"); } }
+			// chave no final de tudo pois o primeiro else envolve todo trecho
 
-                 while (getchar() != '\n');}
+                 while (getchar() != '\n');} 
+		     //o loop infinito getchar leitura char até a novo char da prox linha, por isso != \n
                  break;
-               
 
             case 3:
                 printf("Você escolheu Apagar.\n");
@@ -180,43 +176,48 @@ int main() {
                 ficha[hora].codigo = 0;
                 autopreencher(hora);
                 break;
+
             case 4:
                 printf("Você escolheu Alterar.\n");
                 
-                int escolha;
+		int escolha;
 
                 printf("\nSelecione o convênio que deseja alterar o valor:\n");
-    
+
                 for(int i = 0; i < 4; i++){
-                   printf("%d. %s\n", i+1, convenio[i].nome);
+                   printf("%d. %s - %d\n", 
+			   i+1, 
+			   convenio[i].nome, 
+			   convenio[i].codigo);
                 }
 
                 printf("Escolha: ");
                 scanf("%d", &escolha);
-                
-                if(escolha >= 1 && escolha <= 4) {               
-                
+
+                if(escolha >= 1 && escolha <= 4) {
+
                 int novoValor;
 
                 printf("Novo valor: ");
                 scanf("%d", &novoValor);
-                
+
                 //[escolha - 1]  por causa da indexação causada de 1 a 4 e não de 0 a 3!
                 convenio[escolha - 1].valor = novoValor;
 
                 printf("Valores do convênio %s alterados com sucesso!\n", convenio[escolha - 1].nome);}
-                 
-                else { printf("Opção inválida.\n");
-                     } break;
 
+                else { printf("Opção inválida.\n"); } 
+		break;
+ 
             case 5:
                 printf("Saindo do programa.\n");
                 break;
-            default:
+                
+		default:
                 printf("Opção inválida. Tente novamente.\n");
                 break;
         }
 
     } while (escolha != 5);
 
-   return 0;}
+   return 0; }
